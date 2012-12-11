0336a8964ca4c2c4e3b08c0b0ac5448525c13da2
Dwight Matthys <dwight.matthys@gmail.com>
2012-12-11 22:52:48 +0100

    Fireball bounce impl started.

--- Assets/2D Mario Assets/Scripts/projectileFireball.js
+++ Assets/2D Mario Assets/Scripts/projectileFireball.js
@@ -1,5 +1,48 @@
+var moveSpeed : float = 1.0;
+var bounceHeight : float = 0.25;
+var lifeSpan : float = 2.0;
+var hitPosition : float = 0.0;
+var bounceUp : boolean = false;
+var heightDifference : float = 0.0;
 
-function Update ()
+
+function Start()
 {
+}
 
+
+function Update()
+{
+	if (bounceUp)
+	{
+		transform.Translate(moveSpeed * Time.deltaTime, 0.75 * Time.deltaTime, 0);
+		heightDifference = transform.position.y - hitPosition;
+		if (bounceHeight <= heightDifference)
+		{
+			bounceUp = false;
+		}
+	}
+	else
+	{
+		transform.Translate(moveSpeed * Time.deltaTime, -1.0 * Time.deltaTime, 0);
+	}
+
+	//var ySpeedFactor = bounceUp ? 0.75 : -1.0;
+	//transform.Translate(moveSpeed * Time.deltaTime, ySpeedFactor * Time.deltaTime, 0);
+}
+
+
+function OnTriggerEnter(other : Collider)
+{
+	print(other.transform.tag);
+	if (other.transform.tag == "Untagged")
+	{
+		bounceUp = true;
+		hitPosition = transform.position.y;
+	}
+}
+
+
+function KillFireball()
+{
 }
